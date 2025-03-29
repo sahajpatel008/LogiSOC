842289b53f805829e35230bc675b410ba8942ac0
sahajpatel008 <sahajpatel008@gmail.com>
2025-03-29 07:35:01 -0700

    ui changes

--- frontend/src/components/DynamicTable.tsx
+++ frontend/src/components/DynamicTable.tsx
@@ -13,26 +13,42 @@ type Props = {
 
 const DynamicTable: React.FC<Props> = ({ title = 'Table', columns, rows }) => {
   return (
-    <TableContainer component={Paper} sx={{
+    <TableContainer
+    component={Paper}
+    sx={{
       mb: 4,
       maxHeight: 500,
-      overflowY: 'auto'
-    }}>
-      {title && (
-  <Box
-    sx={{
-      position: 'sticky',
-      top: 0,
-      zIndex: 2,
-      backgroundColor: 'white', // match your Paper bg
-      borderBottom: '1px solid #ccc',
-      p: 2,
+      overflowY: 'auto',
+      border: 'none',
+      borderRight: '1px solid #e0e0e0', // this is the only visible border
+      boxShadow: 'none',
     }}
   >
-    <Typography variant="h6">{title}</Typography>
-  </Box>
-)}
-      <Table stickyHeader>
+      {title && (
+        <Box
+          sx={{
+            position: 'sticky',
+            top: 0,
+            zIndex: 2,
+            backgroundColor: 'white', // match your Paper bg
+            borderBottom: '1px solid #ccc',
+            p: 2,
+          }}
+        >
+          <Typography variant="h6">{title}</Typography>
+        </Box>
+      )}
+      <Table
+        stickyHeader
+        sx={{
+          border: 'none',
+          '& td, & th': {
+            border: 'none',
+          },
+        }}
+      >
+
+
         <TableHead>
           <TableRow>
             {columns.map((col, idx) => (
